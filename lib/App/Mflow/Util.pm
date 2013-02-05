f66646fcaa0840d94f30e98032d3e883f04398ef
Tommy Stanton <tommystanton@gmail.com>
2013-02-05 15:44:45 -0600

    Add svn_status() to *::Util, using SVN::Client

--- lib/App/Mflow/Util.pm
+++ lib/App/Mflow/Util.pm
@@ -2,14 +2,17 @@ package App::Mflow::Util;
 use Moose;
 
 use MooseX::Params::Validate qw( validated_list );
-use MooseX::Types::Path::Class::MoreCoercions qw( Dir );
+use MooseX::ClassAttribute;
+use MooseX::Types::Path::Class::MoreCoercions qw( AbsDir Dir );
 
+use SVN::Client ();
+use Path::Class ();
 use File::chdir;
 use IO::CaptureOutput qw( capture_exec );
 use Text::Wrap qw( wrap ); $Text::Wrap::columns = 72;
 
 sub svn_functions {
-    map { "svn_${_}" } qw( checkout add mkdir commit revert );
+    map { "svn_${_}" } qw( checkout status add mkdir commit revert );
 }
 
 use Sub::Exporter -setup => {
@@ -24,6 +27,44 @@ $Log::Minimal::PRINT = sub {
     my ( $time, $type, $message, $trace, $raw_message ) = @_;
     print "$type: $message\n";
 };
+
+class_has _svn => (
+    is      => 'ro',
+    isa     => 'SVN::Client',
+    default => sub { SVN::Client->new },
+    lazy    => 1,
+);
+
+class_has _svn_statuses => (
+    is         => 'ro',
+    isa        => 'HashRef',
+    lazy_build => 1,
+);
+
+sub _build__svn_statuses {
+    my ($self) = @_;
+
+    # These are listed in the CONSTANTS section of the documentation for
+    # SVN::Wc
+    my @status_names = qw(
+        added conflicted deleted external ignored incomplete
+        merged missing modified none normal obstructed
+        replaced unversioned
+    );
+
+    my %statuses;
+    foreach my $status_name (@status_names) {
+        my $status_code;
+        {
+            no strict 'refs';
+            $status_code = ${"SVN::Wc::Status::${status_name}"};
+        }
+
+        $statuses{$status_code} = $status_name;
+    }
+
+    return \%statuses;
+}
 
 sub svn_checkout {
     my ( $url, $to_path, $dry_run ) = validated_list(
@@ -54,6 +95,33 @@ sub svn_checkout {
     }
 
     return 0;
+}
+
+sub svn_status {
+    my ($abs_path) = validated_list(
+        \@_,
+        path => { isa => AbsDir, coerce => 1 },
+    );
+
+    my %statuses;
+    __PACKAGE__->_svn->status(
+        $abs_path->stringify,
+        'HEAD',
+        sub {
+            my ( $svn_path, $status ) = @_;
+
+            my $status_code = $status->text_status;
+            my $status_name = __PACKAGE__->_svn_statuses->{$status_code};
+
+            my $rel_path = Path::Class::File->new($svn_path)
+                                            ->relative($abs_path);
+
+            $statuses{$rel_path} = $status_name;
+        },
+        0, 1, 1, 0
+    );
+
+    return \%statuses;
 }
 
 sub svn_mkdir {
@@ -182,6 +250,7 @@ EOT
 }
 
 no Moose;
+no MooseX::ClassAttribute;
 
 __PACKAGE__->meta->make_immutable;
 
